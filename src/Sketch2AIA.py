dd10cd1e746439fbd25fae5fb95e8bb3ca301ba3
Daniel Baulé <dsbaule@gmail.com>
2020-05-27 22:22:08 -0300

    Bootstrap website

---
+++ src/Sketch2AIA.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, request, render_template, send_from_directory
+
+__author__ = 'Daniel Baulé'
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+@app.route("/")
+def index():
+    return render_template("base.html")
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/newsketch")
+def newsketch():
+    return render_template("newsketch.html")
+
+if __name__ == "__main__":
+    app.run(port=4555, debug=True)
